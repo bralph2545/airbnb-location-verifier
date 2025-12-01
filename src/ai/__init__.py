f918e0d9a50bf9419e19f0ecc6955405b484cc46
bralph2545 <brett@portoro.com>
2025-12-01 10:11:36 -0500

    Add src/ai/__init__.py

---
+++ src/ai/__init__.py
@@ -0,0 +1,11 @@
+"""AI and machine learning modules"""
+
+from .ai_helpers import (
+    generate_neighborhood_insights,
+    analyze_property_description
+)
+
+__all__ = [
+    'generate_neighborhood_insights',
+    'analyze_property_description'
+]
